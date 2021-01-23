85a649e144c5c842c6c6b4b54bbf030992fe6e90
Chidi Offor <chidioffor@yahoo.com>
2021-01-23 19:52:51 +0000

    Added delete method, view, updated links in index, show, edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 root 'pages#home'
 get 'about', to: 'pages#about'
 resources :articles
-#resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+# below has same effect as (resources :articles), 
+# but shows how to selectively expose rails routes 
+#resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] 
 end
