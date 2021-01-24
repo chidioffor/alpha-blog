38a985834358739be02456c94ac0a6e9c3c43fd1
Chidi Offor <chidioffor@yahoo.com>
2021-01-24 14:17:12 +0000

    DRYied out the codes in articles controller and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+	
+	before_action :select_article, only: [:show, :edit, :update, :destroy]
+
 	def show
-	  @article = Article.find(params[:id])
+
 	end
 
 	def index
@@ -12,12 +15,12 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-	  @article = Article.find(params[:id])
+
 	end
 
 	def create
 	  #render plain: params[:article]
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was successfully saved"
 			redirect_to @article
@@ -29,8 +32,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-	  @article = Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(article_params)
 			flash[:notice] = "Article was successfully saved"
 			redirect_to @article
 		else 
@@ -39,9 +41,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy 
 		redirect_to articles_path
+	end
+
+	private
+
+	def select_article
+	  @article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)	
 	end
 
 end
