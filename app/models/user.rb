0705b59e6a4699058cfcfd1046c8a6f105b45e15
Chidi Offor <chidioffor@yahoo.com>
2021-01-28 12:59:33 +0000

    + user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+	validates :username, 
+					presence: true, 
+					length: { minimum: 4, maximum: 25 },
+					uniqueness: { case_sensitive: false }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+	validates :email,
+					presence: true,
+					uniqueness: { case_sensitive: false },
+					format: { with: VALID_EMAIL_REGEX }
 	
 end
